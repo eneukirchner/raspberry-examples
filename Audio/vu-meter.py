14d52a89598c30ee318ceb0561dc56655412ed35
eneukirchner <nr@bulme.at>
2014-11-03 19:24:00 +0100

    gelöscht:               Audio/test.wav 	geändert:               Audio/vu-meter.py

--- Audio/vu-meter.py
+++ Audio/vu-meter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Edgar Neukirchner 2014
 
 from PyQt4 import QtGui, QtCore 
 import alsaaudio
@@ -7,13 +8,11 @@ import audioop
 import sys
 import math
 
-class VuMeter(QtGui.QMainWindow):
-        def __init__(self):
-                QtGui.QMainWindow.__init__(self)
-                self.initUI()
-                        
-        def initUI(self):
+class VuMeter(QtGui.QWidget):
+        def __init__(self, parent=None):
+                QtGui.QWidget.__init__(self, parent)
                 self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK, 'sysdefault:CARD=AUDIO')
+                # self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
                 self.inp.setchannels(1)                          
                 self.inp.setrate(8000)                           
                 self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)  
@@ -23,24 +22,26 @@ class VuMeter(QtGui.QMainWindow):
                 self.timer.timeout.connect(self.getPCM)
                 self.timer.start(5)
                 
-                self.bar = QtGui.QProgressBar()
-                self.bar.setMinimum(0)
-                self.bar.setMaximum(32768)
-                self.bar.setTextVisible(0)
+                self.setGeometry(0, 0, 320, 240)
+                self.setWindowTitle('VU Meter')
+                self.bar = QtGui.QProgressBar(self)
+                self.bar.setGeometry(20, 100, 280, 40)
                 
-                self.setCentralWidget(self.bar)
-                self.setGeometry(20, 20, 320, 220)
-                self.setWindowTitle('Audio')
-                
-                self.show()
-        
+                self.steps = float(32768)
+                self.min = 20*math.log10(1/self.steps)
+                self.bar.setMinimum(int(self.min))
+                self.bar.setMaximum(0)
+                self.bar.setFormat("%v dB")
+                                
         def getPCM(self):
                 l, data = self.inp.read()
                 hi = 32768.0
                 if l > 0:
-                        # vu = 20*math.log(max(audioop.max(data, 2),1)/hi)
-                        vu = audioop.max(data,2)
+                        # Dezibel
+                        vu = 20*math.log10(max(audioop.max(data, 2),1)/self.steps)
+                        # vu = audioop.max(data,2)
                         self.bar.setValue(vu)
+                        # print vu
                       
 def main():
         app = QtGui.QApplication(sys.argv)
